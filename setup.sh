66670dec82c2d0ae76012a717da84c2c8006dc82
Dominik Liebmann <dominik_l@gmx.net>
2019-10-04 14:12:44 +0200

    simplified deployment

--- setup.sh
+++ setup.sh
@@ -15,10 +15,24 @@ export NAMESPACE="toolbox"
 oc login -u admin
 oc new-project "${NAMESPACE}"
 
+# INSTALL 3SCALE TOOLBOX
+# Check version
+ruby -v
+# Install the Ruby Version Manager rvm
+curl -L https://get.rvm.io | bash -s stable
+rvm install ruby-2.4.1
+rvm use ruby-2.4.1
+rvm --default use 2.4.1
+gem install 3scale_toolbox
+
+#Generate the 3scale toolbox secret
+#3scale remote add 3scale-saas "https://$SAAS_ACCESS_TOKEN@$SAAS_TENANT-admin.3scale.net/"
+3scale remote add 3scale-onprem "https://$ONPREM_ACCESS_TOKEN@$ONPREM_ADMIN_PORTAL_HOSTNAME/"
+oc create secret generic 3scale-toolbox -n "$NAMESPACE" --from-file="$HOME/.3scalerc.yaml"
+
 # install jenkins and pipeline
 oc new-app -n "$NAMESPACE" --template=jenkins-ephemeral --name=jenkins
 oc set env -n "${NAMESPACE}" dc/jenkins JENKINS_OPTS=--sessionTimeout=86400
-
 oc process -f simple-setup/setup.yaml -p DEVELOPER_ACCOUNT_ID=3 -p PRIVATE_BASE_URL="https://gugus" | oc apply -f - 
 oc start-build simple-setup --follow
 
